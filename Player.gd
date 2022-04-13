56b7b6889a3637de20fa2e2785b0a4469dd8bdc7
Scott Osmundson <scottosmundson@gmail.com>
2022-04-12 22:36:38 -0500

    buggy bhopping

--- Player.gd
+++ Player.gd
@@ -5,6 +5,10 @@ export var gravity  = 70
 export var jump_impulse = 25
 
 var velocity = Vector3.ZERO
+var oldpos = Vector3.ZERO
+var newpos = Vector3.ZERO
+var height = Vector3.ZERO
+var changeheight = Vector3.ZERO
 
 onready var pivot = $Pivot
 
@@ -14,6 +18,8 @@ func _physics_process(delta):
 	apply_gravity(delta)
 	jump()
 	velocity = move_and_slide(velocity, Vector3.UP)
+	
+	
 	
 func get_input_vector():
 	var input_vector = Vector3.ZERO
@@ -27,9 +33,13 @@ func apply_movement(input_vector):
 	velocity.x = input_vector.x * max_speed
 	velocity.z = input_vector.z * max_speed
 	
+	
 	if input_vector != Vector3.ZERO:
 		pivot.look_at(translation + input_vector, Vector3.UP)
 	
+
+func get_position():
+	return self.global_transform.origin
 
 func apply_gravity(delta):
 	velocity.y -= gravity * delta
@@ -37,3 +47,22 @@ func apply_gravity(delta):
 func jump():
 	if is_on_floor() and Input.is_action_pressed("jump"):
 		velocity.y = jump_impulse
+		newpos = self.get_position()
+		if Vector3(-3,0,-3) < newpos - oldpos and newpos - oldpos < Vector3(3,0,3):
+			max_speed += 2
+			print(max_speed)
+			print(oldpos)
+			print(newpos)
+			oldpos = Vector3.ZERO
+		else:
+			max_speed = 12
+			print("didnt work")
+			print(newpos - oldpos)
+			print(oldpos)
+			print(newpos)
+			oldpos = Vector3.ZERO
+	if is_on_floor() and !Input.is_action_pressed("jump"):
+		if oldpos == Vector3.ZERO:
+			oldpos = self.get_position()
+			print("GOT")
+
